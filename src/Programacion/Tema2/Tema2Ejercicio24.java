b14fb62fe62798582345a14c180d75cb7ab65010
Pintura-psd <usuhy1999@gmail.es>
2024-11-07 14:07:47 +0100

    Ejecicios hasta el 24 y me falta el 22

---
+++ src/Programacion/Tema2/Tema2Ejercicio24.java
@@ -0,0 +1,13 @@
+package Programacion.Tema2;
+
+public class Tema2Ejercicio24 {
+    public static void main (String[] arg){
+        /*24. Realiza un programa que permite a un profesor introducir la nota de un
+número indefinido de alumnos y le indica cuántos suspendidos o
+aprobados hay*/
+
+        int alumos;
+        double mark;
+
+    }
+}

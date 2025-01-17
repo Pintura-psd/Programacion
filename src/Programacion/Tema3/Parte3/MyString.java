71363c998175e6717526eef6fa7a9f46196acad2
Fer <usuhy1999@gmail.com>
2025-01-17 14:08:23 +0100

    Esto es lo de las cadenas.

--- src/Programacion/Tema3/Parte3/MyString.java
+++ src/Programacion/Tema3/Parte3/MyString.java
@@ -7,12 +7,14 @@ public class MyString {
 
         String menu = "menu";
         String cadena = " ";
+        String cadena2 = " ";
         Scanner input = new Scanner(System.in);
 
         System.out.println("a: Invertir una palabra.");
-        System.out.println("b: Contar vocales");
+        System.out.println("b: Contar vocales.");
+        System.out.println("c: Devolver la palabra más larga de una string.");
 
-        menu = input.next();
+        menu = input.nextLine();
 
         switch (menu){
             case "a":
@@ -32,7 +34,25 @@ public class MyString {
                 cadena = input.next();
 
                 System.out.println(vowelsCount(cadena));
+                break;
+            case "c":
+                /*c) Una función que reciba una cadena y devuelva la palabra de
+                mayor longitud*/
 
+                System.out.println("Introduce una frase.");
+                cadena = input.nextLine();
+
+                System.out.println("La palabra mas larga es: "+ longWord(cadena));
+                break;
+
+            case "d":
+                /*d) Una función que reciba dos cadenas y devuelva el número de
+                veces que la segunda cadena está incluida en la primera.*/
+
+                System.out.println("Introduce la primera cadena.");
+                cadena = input.nextLine();
+                System.out.println("Introduce la segunda cadena");
+                cadena2 = input.nextLine();
 
                 break;
         }
@@ -40,12 +60,12 @@ public class MyString {
     }
 
 public static String reverseString(String cadena) {
-    String invertida =" ";
+    String invertida ="";
 
     for (int i = cadena.length() - 1; i >= 0; i--) {
         invertida += cadena.charAt(i);
     }
-        return invertida;
+        return invertida.toUpperCase();
 }
 
 public static int vowelsCount(String cadena){
@@ -60,4 +80,30 @@ public static int vowelsCount(String cadena){
         }
         return count;
 }
+
+public static String longWord (String cadena){
+
+        int length = 0;
+        String cadena2 = cadena.toLowerCase();
+
+        String[] empty;
+        String vuelta = " ";
+        empty = cadena2.split(" ");
+
+        for (int i=0; i < empty.length; i++){
+            if (empty[i].length()>length){
+                length = empty[i].length();
+                vuelta = empty[i];
+            }
+        }
+        return vuelta;
+}
+
+public static String repeticionCadena (String cadena1, String cadena2){
+
+
+
+        return cadena1;
+}
+
 }
